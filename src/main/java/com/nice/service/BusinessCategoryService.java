ed7be34628d9d13d8091f641d722f2327307f7f3
nisha.parmar <nisha.parmar@kodytechnolab.com>
2020-06-29 16:48:42 +0530

    added comment

--- src/main/java/com/nice/service/BusinessCategoryService.java
+++ src/main/java/com/nice/service/BusinessCategoryService.java
@@ -10,18 +10,64 @@ import com.nice.exception.ValidationException;
 
 public interface BusinessCategoryService {
 
+	/**
+	 * 
+	 * @param businessCategoryDto
+	 * @return
+	 */
 	public boolean isExists(BusinessCategoryDTO businessCategoryDto) ;
 
+	/**
+	 * 
+	 * @param businessCategoryDto
+	 * @param image
+	 * @return
+	 * @throws NotFoundException
+	 */
 	public BusinessCategoryDTO addBusinessCategory( BusinessCategoryDTO businessCategoryDto, MultipartFile image) throws NotFoundException ;
 
+	/**
+	 * 
+	 * @param businessCategoryDTO
+	 * @param image
+	 * @return
+	 * @throws NotFoundException
+	 * @throws ValidationException
+	 */
 	public BusinessCategoryDTO updateBusinessCategory( BusinessCategoryDTO businessCategoryDTO, MultipartFile image) throws NotFoundException, ValidationException;
 
+	/**
+	 * 
+	 * @param businessCategoryId
+	 * @return
+	 * @throws NotFoundException
+	 */
 	public BusinessCategoryDTO getBusinessCategory(Long businessCategoryId) throws NotFoundException;
 
+	/**
+	 * 
+	 * @param pageNumber
+	 * @param pageSize
+	 * @param activeRecords
+	 * @return
+	 */
 	public Page<BusinessCategory> getList(Integer pageNumber, Integer pageSize, Boolean activeRecords);
 
+	/**
+	 * 
+	 * @param businessCategoryId
+	 * @param active
+	 * @throws ValidationException
+	 * @throws NotFoundException
+	 */
 	public void changeStatus(Long businessCategoryId, Boolean active) throws ValidationException, NotFoundException;
 
+	/**
+	 * 
+	 * @param businessCategoryId
+	 * @return
+	 * @throws NotFoundException
+	 */
 	BusinessCategory getBusinessCategoryDetail(Long businessCategoryId) throws NotFoundException;
 	
 }
--- src/main/java/com/nice/service/SubscriptionPlanService.java
+++ src/main/java/com/nice/service/SubscriptionPlanService.java
@@ -9,18 +9,63 @@ import com.nice.exception.ValidationException;
 
 public interface SubscriptionPlanService {
 
+	/**
+	 * 
+	 * @param subscriptionPlanDto
+	 * @return
+	 */
 	public boolean isExists(SubscriptionPlanDTO subscriptionPlanDto) ;
 
+	/**
+	 * 
+	 * @param subscriptionPlanDto
+	 * @return
+	 * @throws NotFoundException
+	 */
 	public SubscriptionPlanDTO addSubscriptionPlan( SubscriptionPlanDTO subscriptionPlanDto) throws NotFoundException ;
 
+	/**
+	 * 
+	 * @param subscriptionPlanDTO
+	 * @return
+	 * @throws NotFoundException
+	 * @throws ValidationException
+	 */
 	public SubscriptionPlanDTO updateSubscriptionPlan( SubscriptionPlanDTO subscriptionPlanDTO) throws NotFoundException, ValidationException;
 
+	/**
+	 * 
+	 * @param subscriptionPlanId
+	 * @return
+	 * @throws NotFoundException
+	 */
 	public SubscriptionPlanDTO getSubscriptionPlan(Long subscriptionPlanId) throws NotFoundException;
 
+	/**
+	 * 
+	 * @param pageNumber
+	 * @param pageSize
+	 * @param activeRecords
+	 * @param searchKeyWord
+	 * @return
+	 */
 	public Page<SubscriptionPlan> getList(Integer pageNumber, Integer pageSize, Boolean activeRecords, String searchKeyWord);
 
+	/**
+	 * 
+	 * @param subscriptionPlanId
+	 * @param active
+	 * @throws ValidationException
+	 * @throws NotFoundException
+	 */
 	public void changeStatus(Long subscriptionPlanId, Boolean active) throws ValidationException, NotFoundException;
 
+	/**
+	 * 
+	 * @param subscriptionPlanId
+	 * @return
+	 * @throws NotFoundException
+	 */
 	SubscriptionPlan getSubscriptionPlanDetail(Long subscriptionPlanId) throws NotFoundException;
 	
 }
