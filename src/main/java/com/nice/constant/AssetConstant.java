c2e23f42cf8a98888d79942a7fa0dac42f62a61f
Ronit Macwan <ronit.macwan@kodytechnolab.com>
2020-07-30 11:03:57 +0530

    asset constant change and qa property changes.

--- src/main/java/com/nice/constant/AssetConstant.java
+++ src/main/java/com/nice/constant/AssetConstant.java
@@ -17,22 +17,23 @@ public final class AssetConstant {
 	}
 
 	public static final String DIGITAL_ASSET_DOWNLOAD = "digital_asset/downloadFile/";
-	public static final String COMPANY_DIR = "company";
-	public static final String CATEGORY = "category";
-	public static final String SUB_CATEGORY = "sub-category";
-	public static final String BRAND = "brand";
-	public static final String SLIDER_IMAGES = "slider-images";
+	public static final String MAIN_DIR = "nice-staging";
+	public static final String COMPANY_DIR = MAIN_DIR + "/company";
+	public static final String CATEGORY = MAIN_DIR + "/category";
+	public static final String SUB_CATEGORY = MAIN_DIR + "/sub-category";
+	public static final String BRAND = MAIN_DIR + "/brand";
+	public static final String SLIDER_IMAGES = MAIN_DIR + "/slider-images";
 
-	public static final String BUSINESS_CATEGORY_DIR = "business/category";
-	public static final String CUISINE = "cuisine";
-	public static final String DELIVERY_BOY = "delivery-boy";
-	public static final String VENDOR = "vendor";
-	public static final String PRODUCT_DIR = "product";
-	public static final String UOM = "uom";
-	public static final String ADDONS = "addons";
-	public static final String TOPPING = "topping";
-	public static final String EXTRAS = "extras";
-	public static final String ATTRIBUTE = "attribute";
+	public static final String BUSINESS_CATEGORY_DIR = MAIN_DIR + "/business/category";
+	public static final String CUISINE = MAIN_DIR + "/cuisine";
+	public static final String DELIVERY_BOY = MAIN_DIR + "/delivery-boy";
+	public static final String VENDOR = MAIN_DIR + "/vendor";
+	public static final String PRODUCT_DIR = MAIN_DIR + "/product";
+	public static final String UOM = MAIN_DIR + "/uom";
+	public static final String ADDONS = MAIN_DIR + "/addons";
+	public static final String TOPPING = MAIN_DIR + "/topping";
+	public static final String EXTRAS = MAIN_DIR + "/extras";
+	public static final String ATTRIBUTE = MAIN_DIR + "/attribute";
 	public static final boolean IS_AWS = false;
 
 }
