8e382a8cfa125b78d1e70d9dcbd5530987cb367d
ankit_bhimani <ankit.bhimani@kodytechnolab.com>
2020-09-14 13:42:08 +0530

    left in commit

---
+++ src/main/resources/db/changelog/sql-files/1.0/role_permission_new_insert_delete.sql
@@ -0,0 +1,8 @@
+INSERT INTO permission(
+	 active, created_at, created_by, updated_at, updated_by, can_add, can_delete, can_edit, can_view, modules_id, role_id, side_bar)
+	VALUES (true, now(), 1, now(), 1, false, false, false,true, 17, 2,false);
+
+INSERT INTO permission(
+	 active, created_at, created_by, updated_at, updated_by, can_add, can_delete, can_edit, can_view, modules_id, role_id, side_bar)
+	VALUES (true, now(), 1, now(), 1, false, false, false,true, 17, 3,false);
+	
