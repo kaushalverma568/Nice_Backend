da245d80802ab6fec6e38986aaa64d499a96b1df
Het Shah <hetshah@seekadegree.com>
2020-09-14 17:02:44 +0530

    Changeset merged

--- src/main/resources/db/changelog/sql-files/1.0/inventory_changes.sql
+++
@@ -1,8 +0,0 @@
-ALTER TABLE stock_details 
-    ALTER COLUMN  lot_no TYPE numeric(10,2),
-    ALTER COLUMN  available TYPE numeric(10,2),
-    ALTER COLUMN   reserved TYPE numeric(10,2),
-    ALTER COLUMN   delivered TYPE numeric(10,2),
-    ALTER COLUMN   replaced TYPE numeric(10,2),
-    ALTER COLUMN  returned TYPE numeric(10,2),
-    ALTER COLUMN   expired TYPE numeric(10,2);
